d5298d3c0766a006ffede7611b75cb79f0d4fa21
Léa <chanel29_msn@hotmail.fr>
2019-12-17 20:50:54 +0100

    responsive menu 80% fait

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
-// $(document).ready(function(){
+$(document).ready(function(){
+
+    var toggle = document.querySelector(".nav-toggle");
+    var menu = document.querySelector("#header_nav");
+    var icon = document.querySelector("#header_nav a.logo");
+
+    toggle.innerHTML = '<i class="fas fa-bars"></i>';
+    function doToggle() {
+        menu.classList.toggle("open");
+        toggle.classList.toggle("active");
+        icon.classList.toggle("active");
+    }
+
+    $('.nav-toggle').click(doToggle);
 
 
-// }
+    // $('.nav-toggle').click(function(){
+    //     toggle.html('<i class="fas fa-bars"></i>');
+    // },
+    // function(){
+    //     toggle.html('<i class="fas fa-times"></i>');
+    // });
+
+});
