d2b76c94220d4305c5a2c731fdd4a83c0133c651
Léa <chanel29_msn@hotmail.fr>
2019-12-19 15:16:20 +0100

    renommage classe, background gradient img, animation

--- js/script.js
+++ js/script.js
@@ -3,17 +3,20 @@ $(document).ready(function(){
     // Menu
 
     var toggle = document.querySelector(".nav-toggle");
-    var menu = document.querySelector("#header_nav");
-    var icon = document.querySelector("#header_nav a.logo");
 
     toggle.innerHTML = '<i class="fas fa-bars"></i>';
     function doToggle() {
-        menu.classList.toggle("open");
-        toggle.classList.toggle("active");
-        icon.classList.toggle("active");
+        $("#header_nav").toggleClass("open");
+        $(".nav-toggle").toggleClass("active");
+        $("#header_nav a.logo").toggleClass("active");
     }
 
     $('.nav-toggle').click(doToggle);
+    
+    // Carousel Card Info
 
+    $("section.ws-right .ws-info").hover(function(){
+        $(this).toggleClass("active");
+    });
 
 });
