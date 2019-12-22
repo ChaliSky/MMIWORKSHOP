a7877199a1db0f6d245bd62211539ed18ae562ed
Léa <chanel29_msn@hotmail.fr>
2019-12-22 17:32:57 +0100

    fix nav soulignement + gradient linear sur carousel

--- js/script.js
+++ js/script.js
@@ -1,23 +1,32 @@
 $(document).ready(function(){
 
+    // Redimenssionnement texte 
+
+    $('#title').css('font-size', $(window).width() / 32.5);
+    $(window).resize(function(){
+      $('#title').css('font-size', $(window).width() / 32.5);
+    });
+
     // Menu hamburger format mobile
 
     var toggle = document.querySelector(".nav-toggle");
     var menu = document.querySelector(".nav_principale");
-
     //La fonction permettent de basculer l'affichage en ajoutant/supprimant des classes
     function doToggle(){
       menu.classList.toggle("open");
       toggle.classList.toggle("active");
     }
-
-
     toggle.addEventListener('click', doToggle);
 
     // Carousel Card Info
-
     $("section.ws-right .ws-info").hover(function(){
         $(this).toggleClass("active");
     });
 
+
+    // Nav soulignement orange 
+
+    $("header > #header_nav ul li a").hover(function(){
+      $(this).children().toggleClass("active");
+    })
 });
